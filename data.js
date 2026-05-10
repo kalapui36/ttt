7bec62aabd405d0cd571f2e7ad2de19e2013e947
kalapui36 <kalapui36@gmail.com>
2026-05-10 13:51:42 +0900

    Update data.js

--- data.js
+++ data.js
@@ -4,64 +4,64 @@ const manualData = [
     id: 1,
     title: "업무포털 안내",
     description: "서울특별시교육청 업무포털 첫 화면 접속 방법 및 주요 메뉴 안내",
-    thumbnail: "t1.png",
-    link: "m1.html"
+    thumbnail: "t01.png",
+    link: "m01.html"
   },
   {
     id: 2,
     title: "나이스 조퇴 상신",
     description: "나이스 시스템에서 조퇴 상신을 하고 결재선을 지정하는 기본 방법",
-    thumbnail: "t2.png",
-    link: "m2.html"
+    thumbnail: "t02.png",
+    link: "m02.html"
   },
   {
     id: 3,
     title: "나이스 출장 상신",
     description: "관내 및 관외 출장 상신, 여비 지급 여부 체크 및 결재 요청 방법",
-    thumbnail: "t3.png",
-    link: "m3.html"
+    thumbnail: "t03.png",
+    link: "m03.html"
   },
   {
     id: 4,
     title: "내부메일 확인 및 작성",
     description: "에듀파인 내부메일 수신함 확인, 수신자 검색 및 첨부파일을 포함한 메일 작성법",
-    thumbnail: "t4.png",
-    link: "m4.html"
+    thumbnail: "t04.png",
+    link: "m04.html"
   },
   {
     id: 5,
     title: "문서 환경 가로 설정",
     description: "결재선과 본문을 한 화면에서 보기 편하게 개인문서환경을 가로로 설정하는 팁",
-    thumbnail: "t5.png",
-    link: "m5.html"
+    thumbnail: "t05.png",
+    link: "m05.html"
   },
   {
     id: 6,
     title: "기안문 작성",
     description: "에듀파인 일반기안문 서식을 열어 본문을 작성하고 결재자를 지정하는 과정",
-    thumbnail: "t6.png",
-    link: "m6.html"
+    thumbnail: "t06.png",
+    link: "m06.html"
   },
   {
     id: 7,
     title: "공문서 작성법",
     description: "1., 가., 1) 등 공문서 항목 기호 순서 및 들여쓰기 기본 표기 방법",
-    thumbnail: "t7.png",
-    link: "m7.html"
+    thumbnail: "t07.png",
+    link: "m07.html"
   },
   {
     id: 8,
     title: "외부 수신자 지정",
     description: "타 기관 발송 시 조직도 및 행안부유통을 통해 외부 수신처를 지정하는 방법",
-    thumbnail: "t8.png",
-    link: "m8.html"
+    thumbnail: "t08.png",
+    link: "m08.html"
   },
   {
     id: 9,
     title: "지출 품의",
     description: "에듀파인에서 지출 품의 목록을 작성하고 요구 금액을 입력하여 상신하는 방법",
-    thumbnail: "t9.png",
-    link: "m9.html"
+    thumbnail: "t09.png",
+    link: "m09.html"
   },
   {
     id: 10,
